1aab541e68ca061814220214cb8672c2e5e9f912
clydepogi <47465444+clydepogi@users.noreply.github.com>
2019-05-04 16:00:53 +0800

    Add files via upload

---
+++ Problem 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int A = 1;
+    for (A=1;A<=10;A++)
+    cout << A << endl;
+    system("PAUSE");
+    for (A=10;A<=30;A+=2)
+    cout << A << endl;
+system("PAUSE");
+_getch();
+return 0;
+}
