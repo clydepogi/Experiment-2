08e1e8464213ab51d8a1efeb5631c59dc5888489
clydepogi <47465444+clydepogi@users.noreply.github.com>
2019-05-04 16:33:54 +0800

    Add files via upload

---
+++ Problem 7.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<conio.h>
+#include<cmath>
+#include<string>
+using namespace std;
+int main()
+{
+	int X,I, SUM=0;
+	
+	
+	{
+	cout<<"Please encode a number"<<endl;
+	cin>> X;
+	for(int I = 0; I <= X; I++)
+	{
+	SUM+=I;
+	}
+	cout<<SUM<<endl;
+	}
+	while(X>0);
+		
+		
+	{
+	cout<<"THANKS"<<endl;
+	system("PAUSE");
+								
+} 
+	while(X<=0);
+	_getch();
+	return 0;
+}
